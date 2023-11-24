4c61230487bd9c8025d7ed9e8db5aa948baac5e9
Dasajton <david.sajitz.94@gmail.com>
2023-11-24 21:54:43 +0100

    first commit

---
+++ src/pages/Cooperation.jsx
@@ -0,0 +1,11 @@
+
+
+const Cooperation = () => {
+    return (
+        <div>
+            <h1>Cooperation</h1>
+        </div>
+    )
+}
+
+export default Cooperation;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+
+const Home = () => {
+    return (
+        <div className="container mx-auto mt-10">
+      <h1 className="text-4xl font-bold mb-6">Willkommen bei HTQ Unternehmensberatung</h1>
+      <p className="text-lg">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </div>
+    )
+}
+
+export default Home;
