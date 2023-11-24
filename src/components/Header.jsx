f362c38891f9b5bed7473cfd093aa0e9c6fd6448
Dasajton <david.sajitz.94@gmail.com>
2023-11-25 00:07:52 +0100

    create dropdownmenu (not finished yet)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,42 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-    return (
-        <>
-        <nav className="navbar w-full h-[5rem] px-4 flex justify-between items-center shadow-sm shadow-green-600">
-        <Link to="/"><span className="logo text-6xl font-bold text-green-700">HTQ</span></Link>
-        <ul className="navbar-list flex gap-6 text-xl">
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="navbar w-full h-[5rem] px-4 flex justify-between items-center shadow-sm shadow-green-600 relative">
+      <Link to="/">
+        <span className="logo text-5xl font-bold text-green-700">HTQ</span>
+      </Link>
+      <ul className="navbar-list hidden gap-6 text-xl md:flex md:items-center">
           <Link to="/about-us" className="py-2 px-4 bg-green-500 hover:bg-green-600 transition text-green-50 rounded">Über Uns</Link>
           <Link to="/services" className="py-2 px-4 bg-green-500 hover:bg-green-600 transition text-green-50 rounded">Unser Service</Link>
           <Link to="/cooperation" className="py-2 px-4 bg-green-500 hover:bg-green-600 transition text-green-50 rounded">Kooperation</Link>
           <Link to="/contact" className="py-2 px-4 bg-green-500 hover:bg-green-600 transition text-green-50 rounded">Kontakt</Link>
         </ul>
-      </nav>
-      
-      </>
-    );
-}
+      <button className="dropdown-btn md:hidden cursor-pointer" onClick={toggleMenu}>
+        {isMenuOpen ? (
+          <img src="./close-icon.svg" alt="Close Menu" className="close-menu" />
+        ) : (
+          <img src="/burger-icon.svg" alt="Burger Menu" className="burger-menu" />
+        )}
+      </button>
+      {isMenuOpen && (
+        <div className="dropdown-menu flex flex-col items-center justify-evenly bg-green-500 border border-green-700 rounded shadow-sm shadow-green-700 mx-auto py-2 w-3/4 absolute z-50">
+          <Link to="/" className="py-6 w-3/4 bg-green-700 hover:bg-green-800 rounded text-green-50 text-xl text-center">Home</Link>
+          <Link to="/about-us" className="py-6 w-3/4 bg-green-700 hover:bg-green-800 rounded text-green-50 text-xl text-center">Über Uns</Link>
+          <Link to="/services" className="py-6 w-3/4 bg-green-700 hover:bg-green-800 rounded text-green-50 text-xl text-center">Unser Service</Link>
+          <Link to="/cooperation" className="py-6 w-3/4 bg-green-700 hover:bg-green-800 rounded text-green-50 text-xl text-center">Kooperation</Link>
+          <Link to="/contact" className="py-6 w-3/4 bg-green-700 hover:bg-green-800 rounded text-green-50 text-xl text-center">Kontakt</Link>
+        </div>
+      )}
+    </nav>
+  );
+};
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@
 
 const Home = () => {
     return (
-        <div className="container mx-auto mt-10">
-      <h1 className="text-4xl font-bold mb-6">Willkommen bei HTQ Unternehmensberatung</h1>
+        <div className="container mt-10 mx-auto px-2">
+      <h1 className="text-3xl md:text-4xl  font-bold mb-6">Willkommen bei HTQ Unternehmensberatung</h1>
       <p className="text-lg">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       </p>
