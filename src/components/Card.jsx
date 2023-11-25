179de972966753ef15416218a192e121f278f156
Dasajton <david.sajitz.94@gmail.com>
2023-11-25 23:19:39 +0100

    create dards component

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+
+
+const Card = (props) => {
+    return (
+        <div className="card bg-blue-100 rounded-lg flex flex-col items-center shadow-sm shadow-blue-600">
+            <img className="rounded-t-lg h-80 w-full object-fill" src={props.image} alt={props.title} />
+            <div className="flex flex-col justify-center items-center">
+            <h2 className="text-2xl sm:text-2xl xl:text-3xl font-medium text-blue-700 p-2">{props.title}</h2>
+            <p className="text-lg overflow-hidden p-2">{props.description}</p>
+            </div>
+            <button className="p-4 my-6 bg-blue-600 hover:bg-blue-700 rounded text-blue-50">Mehr erfahren</button>
+
+
+        </div>
+    )
+}
+
+export default Card;
