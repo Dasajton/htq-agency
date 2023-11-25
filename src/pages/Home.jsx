34e4060e63890709f1f8832f8a3843461e763d8d
Dasajton <david.sajitz.94@gmail.com>
2023-11-25 01:52:34 +0100

    changed dropdown h from vh to rem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@
 const Home = () => {
     return (
         <div className="container mt-10 mx-auto px-2">
-      <h1 className="text-3xl md:text-4xl  font-bold mb-6">Willkommen bei HTQ Unternehmensberatung</h1>
+      <h1 className="text-3xl md:text-4xl font-bold mb-6">Willkommen bei HTQ Unternehmensberatung</h1>
       <p className="text-lg">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       </p>
