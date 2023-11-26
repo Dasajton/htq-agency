d10b7c04550db080547de644522311da7e298973
Dasajton <david.sajitz.94@gmail.com>
2023-11-26 21:37:04 +0100

    create Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <>
+    <footer className="bg-blue-600 text-blue-50 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <p>&copy; 2023 HTQ Consulting</p>
+        <div className="flex gap-4">
+          <Link to="/impressum">Impressum</Link>
+          <Link to="/datenschutz">Datenschutz</Link>
+          <Link to="/contact">Kontakt</Link>
+        </div>
+      </div>
+    </footer>
+    </>
+  );
+};
+
+export default Footer;
