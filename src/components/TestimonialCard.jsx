6e0c80ea57bba0e8a19f85b8e27afbf2512213ae
Dasajton <david.sajitz.94@gmail.com>
2023-11-26 15:09:32 +0100

    create TestimonialCard.jsx

---
+++ src/components/TestimonialCard.jsx
@@ -0,0 +1,15 @@
+
+
+const TestimonialCard = ({ name, role, comment }) => {
+  return (
+    <div className="testimonial-card bg-blue-600 p-6 m-2 rounded-lg shadow-md">
+      <p className="text-white text-lg mb-4">{comment}</p>
+      <div className="flex items-center">
+        <p className="text-blue-50 text-lg font-semibold">{name}</p>
+        <p className="text-blue-200 ml-2">{role}</p>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialCard;
