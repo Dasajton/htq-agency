3e0234de610bbf44d8da725954b05402dd6b796f
Dasajton <david.sajitz.94@gmail.com>
2023-11-27 21:11:31 +0100

    optimize cards (btn to same line position)

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,22 @@
-import ButtonDefault from "./ButtonDefault";
+import ButtonDefault from "./ButtonDefault"
 
 const Card = (props) => {
     return (
         <div className="card bg-blue-100 rounded-lg flex flex-col items-center shadow-sm shadow-blue-600">
-            <img className="rounded-t-lg h-96 w-full object-fill" src={props.image} alt={props.title} />
-            <div className="flex flex-col justify-center items-center">
-            <h2 className="text-2xl sm:text-2xl xl:text-3xl font-medium text-blue-700 p-2">{props.title}</h2>
-            <p className="text-lg p-2">{props.description}</p>
+            <img
+                className="rounded-t-lg h-96 w-full object-cover"
+                src={props.image}
+                alt={props.title}
+            />
+            <div className="flex flex-col flex-1 justify-center items-center">
+                <h2 className="text-2xl sm:text-2xl xl:text-3xl font-medium text-blue-700 p-2">
+                    {props.title}
+                </h2>
+                <p className="text-lg p-2">{props.description}</p>
             </div>
             <ButtonDefault to={props.to} text={props.buttonText} />
-
-
         </div>
     )
 }
 
-export default Card;
+export default Card
