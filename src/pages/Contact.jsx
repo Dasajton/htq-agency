f6c0badfd6525926b45bb0031bb37e990ecb1a37
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-28 22:15:23 +0100

    added contact2

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 const Contact = () => {
     return (
         <div>
-            <h1 className="mt-24 text-3xl font-bold text-center">Kontakt</h1>
+            <h1 className="mt-24 text-3xl font-bold text-center">Kontakt2</h1>
         </div>
     )
 }
